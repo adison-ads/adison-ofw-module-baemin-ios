8095109909c3380a735afdc54a48f539d29fb6b2
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2024-02-14 18:24:52 +0900

    0.9.0-beta1 릴리즈

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let repoURL = "https://github.com/adison-ads/adison-ofw-module-baemin-ios/releases/download"
 let artifactVersion = "0.9.0-beta1"
 let artifactName = "BaeminOfwModule.zip"
-let checksum = "5d31230cb15f9267fd895beef5296c809574d9ed6d3fd14dda3c746c085ebf5f"
+let checksum = "3c8dba12f9ce50a239d665c33bae5753eb89d89669d034b8e0382eab49ff826a"
 
 let offerwallRepoURL = "https://github.com/adison-ads/adison-offerwall-ios-sdk"
 let offerwallExactVersion = Version("3.4.0-beta1")
